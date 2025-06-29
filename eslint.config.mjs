f5171dc33af6772ecd3c2001730632e3d411c6aa
Juan Royo de Barrio <juanroyofullstack@gmail.com>
2025-06-29 17:43:14 +0200

    add: eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,69 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import pluginNext from '@next/eslint-plugin-next'
+import pluginTypeScript from '@typescript-eslint/eslint-plugin';
+import parserTypeScript from '@typescript-eslint/parser';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+export default [
+  {
+    ignores: [
+      'node_modules',
+      '.next',
+      'out',
+      'dist',
+      'build',
+      'coverage',
+      'public',
+      '.turbo',
+      '.vercel',
+      '.git',
+      '**/*.d.ts'
+    ],
+  },
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      parser: parserTypeScript,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      '@next/next': pluginNext,
+      '@typescript-eslint': pluginTypeScript,
+    },
+    rules: {
+      ...pluginNext.configs.recommended.rules,
+      ...pluginNext.configs['core-web-vitals'].rules,
+      'no-console': 'warn',
+      'indent': [
+        'error',
+        4,
+        {
+          'SwitchCase': 1,
+          'VariableDeclarator': 1,
+          'outerIIFEBody': 1,
+          'MemberExpression': 1,
+          'FunctionDeclaration': { 'parameters': 1, 'body': 1 },
+          'FunctionExpression': { 'parameters': 1, 'body': 1 },
+          'CallExpression': { 'arguments': 1 },
+          'ArrayExpression': 1,
+          'ObjectExpression': 1,
+          'ImportDeclaration': 1,
+          'flatTernaryExpressions': false,
+          'offsetTernaryExpressions': true,
+          'ignoreComments': false
+        }
+      ],
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single', { 'avoidEscape': true }],
+      'comma-dangle': ['error', 'always-multiline'],
+      'no-unused-vars': 'warn',
+      '@typescript-eslint/no-unused-vars': ['warn'],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+    }
+  }
+]
