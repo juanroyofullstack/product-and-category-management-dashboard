786378e8ee4f7623c0de4bf16706639ded1305fc
Juan Royo de Barrio <juanroyofullstack@gmail.com>
2025-06-30 13:12:39 +0200

    add: jest config

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
+ 
+const createJestConfig = nextJest({
+    dir: './',
+});
+ 
+const config: Config = {
+    coverageProvider: 'v8',
+    testEnvironment: 'jest-environment-jsdom',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+    transformIgnorePatterns: [
+        '/node_modules/',
+    ],
+    moduleNameMapper: {
+        '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+        '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+        '^.+\\.(jpg|jpeg|png|gif|webp|avif|svg)$': '<rootDir>/__mocks__/fileMock.js',
+    },
+};
+ 
+export default createJestConfig(config);
